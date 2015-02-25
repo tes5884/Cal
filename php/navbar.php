dc309ba842db7862cc86608d2b792c4f464be0e7
tzvi <tes5884@gmail.com>
2015-02-24 23:43:03 -0500

    added

---
+++ php/navbar.php
@@ -0,0 +1,40 @@
+<?php
+include ('init.php');
+function echoActiveClassIfRequestMatches($requestUri)
+{
+    $current_file_name = basename($_SERVER['REQUEST_URI'], ".php");
+
+    if ($current_file_name == $requestUri)
+        echo 'class="active"';
+}
+?>
+
+<?php if($_SESSION['loggedin'] ==1 ): ?>
+	<nav class="navbar navbar-default">
+		<ul class="nav navbar-nav">
+			<li <?=echoActiveClassIfRequestMatches("main")?>><a href="main.php">Main</a>
+			</li>
+			<li <?=echoActiveClassIfRequestMatches("halacha")?>><a href="halacha.php">Halacha</a>
+			</li>
+			<li <?=echoActiveClassIfRequestMatches("contact")?>><a href="contact.php">Contact</a>
+			</li>
+			<li><a href="logout.php">Logout</a>
+			</li>
+		</ul>
+	</nav>
+<?php else: ?>
+	<nav class="navbar navbar-default">
+
+		<ul class="nav navbar-nav">
+			<li <?=echoActiveClassIfRequestMatches("index")?>><a href="index.php">Welcome</a>
+			</li>
+			<li <?=echoActiveClassIfRequestMatches("halacha")?>><a href="halacha.php">Halacha</a>
+			</li>
+			<li <?=echoActiveClassIfRequestMatches("contact")?>><a href="contact.php">Contact</a>
+			</li>
+			<li <?=echoActiveClassIfRequestMatches("login")?>><a href="login.php">Login</a>
+			</li>
+			<li <?=echoActiveClassIfRequestMatches("signup")?>><a href="signup.php">Sign up</a></li>
+		</ul>
+	</nav>
+<?php endif; ?>
