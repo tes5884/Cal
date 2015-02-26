58c7f578b30fc274d608482619ed170a06e7844b
tzvi <tes5884@gmail.com>
2015-02-26 17:21:31 -0500

    wrote update script

--- php/flowUpdate.php
+++ php/flowUpdate.php
@@ -1,3 +1,12 @@
 <?php
-
+  include ('init.php');
+  
+  $date = $_POST['datePicker'];
+  $nightOrDay = $_POST['inlineRadioOptions'];
+  
+  $flowUpdate -> bindParam(':flowDate', $date);
+  $flowUpdate -> bindParam(':dayOrNight', $nightOrDay);
+  $flowUpdate -> bindParam(':userId', $_SESSION['userId']);
+	$flowUpdate -> execute();
 ?>
+<meta http-equiv="refresh" content="0;../main.php">
