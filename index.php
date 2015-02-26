033953c063482a7c55859107966c6f7b1c6e0eb4
tzvi <tes5884@gmail.com>
2015-02-25 20:36:46 -0500

    background img \& blurbs

--- index.php
+++ index.php
@@ -7,11 +7,40 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
 <link rel="stylesheet" href="css/main.css">
 </head>
-<body>
+<body class="full">
 	<div class="container">
   	<!-- Navbar -->
 		<?php include('php/navbar.php');?>
 	<!-- end navbar -->
+	<!-- start grid -->
+	<div class="row">
+	  <div class="col-md-12">
+      <div class="jumbotron">
+        
+      </div>
+	  </div>
+	</div>
+	<div class="row">
+	  <div class="col-md-4">
+	    <p class="lead">Alpha</p>
+	    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam mi mauris, condimentum quis mollis ornare, lacinia quis ligula. Proin eu nulla sapien. Morbi maximus nisi et nunc dictum, eu fermentum ligula euismod. Phasellus iaculis risus et commodo aliquam. Nunc facilisis tincidunt fringilla. Aliquam ullamcorper laoreet arcu ac laoreet. Donec malesuada, nibh a tincidunt condimentum, metus lacus imperdiet magna, ac eleifend velit urna eu elit. In cursus felis eget ante accumsan pharetra. Suspendisse vitae mattis leo, at bibendum turpis. Cras finibus cursus metus varius luctus. Sed risus eros, cursus in mi in, aliquet tempor nisi. Nunc rutrum lacus nec elit dictum, at vulputate mi finibus.</p>
+	    <a href="#" class="btn btn-default">Read more</a>
+	  </div>
+	  <div class="col-md-4">
+	    <p class="lead">Beta</p>
+	    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam mi mauris, condimentum quis mollis ornare, lacinia quis ligula. Proin eu nulla sapien. Morbi maximus nisi et nunc dictum, eu fermentum ligula euismod. Phasellus iaculis risus et commodo aliquam. Nunc facilisis tincidunt fringilla. Aliquam ullamcorper laoreet arcu ac laoreet. Donec malesuada, nibh a tincidunt condimentum, metus lacus imperdiet magna, ac eleifend velit urna eu elit. In cursus felis eget ante accumsan pharetra. Suspendisse vitae mattis leo, at bibendum turpis. Cras finibus cursus metus varius luctus. Sed risus eros, cursus in mi in, aliquet tempor nisi. Nunc rutrum lacus nec elit dictum, at vulputate mi finibus.</p>
+      <a href="#" class="btn btn-default">Read more</a>	  
+	  </div>
+	  <div class="col-md-4">
+	    <p class="lead">Gama</p>
+	    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam mi mauris, condimentum quis mollis ornare, lacinia quis ligula. Proin eu nulla sapien. Morbi maximus nisi et nunc dictum, eu fermentum ligula euismod. Phasellus iaculis risus et commodo aliquam. Nunc facilisis tincidunt fringilla. Aliquam ullamcorper laoreet arcu ac laoreet. Donec malesuada, nibh a tincidunt condimentum, metus lacus imperdiet magna, ac eleifend velit urna eu elit. In cursus felis eget ante accumsan pharetra. Suspendisse vitae mattis leo, at bibendum turpis. Cras finibus cursus metus varius luctus. Sed risus eros, cursus in mi in, aliquet tempor nisi. Nunc rutrum lacus nec elit dictum, at vulputate mi finibus.</p>
+      <a href="#" class="btn btn-default">Read more</a>
+	  </div>
+	</div>
+	<div class="row">
+	  <div class="col-md-12"></div>
+	</div>
+	<!-- end grid -->
 	</div>
 		<!-- js files -->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
