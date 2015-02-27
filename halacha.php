81074e74d83965bf59d8613004ad7f1cd2e6ec5f
tzvi <tes5884@gmail.com>
2015-02-27 11:48:41 -0500

    starting to add stuff

--- halacha.php
+++ halacha.php
@@ -15,8 +15,22 @@
 		<!-- Navbar -->
 		<?php include('php/navbar.php');?>
 		<!-- end navbar -->
-		<div>
-		  <p class="lead">Halacha</p>
+		<div class="row">
+			<div class="col-md-3">
+		  	<p class="lead">Vestos</p>
+		  		<ul>
+		  			<li><a href="">Chodesh - וסת החודש</a></li>
+		  			<li><a href="">Haflagah - וסת הפלגה</a></li>
+		  			<li><a href="">Onah Beinoni - עונה בּינונית</a></li>
+		  			<li><a href="">Or Zaruah - אור זרוע</a></li>
+		  		</ul>
+		  </div>
+		  <div class="col-md-9">
+		  	<p><h2>Chodesh - וסת החודש</h2></p>
+		  	<p><h2>Haflagah - וסת הפלגה</h2></p>
+		  	<p><h2>Onah Beinoni - עונה בּינונית</h2></p>
+		  	<p><h2>Ohr Zaruah - אור זרוע</h2></p>
+		  </div>
 		</div>
 	</div>
 	<!-- js files -->
