ed37f1029720676ec17316d020a990383c2831fb
tzvi <tes5884@gmail.com>
2015-02-27 12:20:36 -0500

    homepage img\'s

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 	<!-- start grid -->
 	<div class="row">
 	  <div class="col-md-12">
-	  <img class="img-responsive" src="img/DSC_0885.jpg">
+	  <img class="img-responsive img-rounded" src="img/test_1.jpg">
       <div class="jumbotron">
       
       </div>
