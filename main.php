0da3e2431095303581513bf7c95ca83cb90ca256
tzvi <tes5884@gmail.com>
2015-03-04 10:50:19 -0500

    fixed issues when no dates in the system

--- main.php
+++ main.php
@@ -31,51 +31,106 @@
 				<p class="lead text-center">Track your mikvah and flow calculations online. </p>
 			</div>
 			<!-- -->
+			
 			<div class="col-md-6 text-center">
+				<!-- upcoming days -->
+				<?php
+				  $latestFlow-> bindParam(':userId', $_SESSION['userId']);
+					$latestFlow-> execute();
+					$data = $latestFlow->fetchObject();
+			
+			  	if (isset($data->flow_date)) {
+			  		$date = json_encode($data->flow_date);
+				  } else {
+				  	$date = json_encode('');
+				  }
+			  ?>
+			  <script>
+			  	if(!<?php echo($date)?> == '') {
+			  		var hebDate = new Hebcal.HDate(new Date(<?php echo($date); ?>));
+			  		var engDate = new Date(<?php echo($date)?>);
+			  	} else {
+			  		document.write('<div class="panel panel-default">Add a flow date, and your data will start showing up.</div>')
+			  	}
+			  </script>
+			  
+			  
+			  
 				<div class="panel panel-default">
 					<div class="panel-heading">Upcoming Days</div>
 					<div class="row">
 						<div class="col-md-4"><strong>Chodesh:</strong></div>
-						<div class="col-md-4">March 21, 1989</div>
-						<div class="col-md-4">Day</div>
+						<div class="col-md-4"><script>if(!<?php echo($date)?> == '') {document.write(hebDate.getMonthObject().next().getDay(hebDate.getDate()).toString());}</script></div>
+						<div class="col-md-4"></div>
 					</div>
 					<div class="row">
 						<div class="col-md-4"><strong>Haflagah:</strong></div>
-						<div class="col-md-4">March 21, 1989</div>
-						<div class="col-md-4">Day</div>
+						<div class="col-md-4"></div>
+						<div class="col-md-4"></div>
 					</div>
 					<div class="row">
 						<div class="col-md-4"><strong>Onah Beinoni:</strong></div>
-						<div class="col-md-4">March 21, 1989</div>
-						<div class="col-md-4">Day</div>
+						<div class="col-md-4">
+							<script>
+								if(!<?php echo($date)?> == '') {
+								engDate.setDate(engDate.getDate() + 29);
+								document.write(Hebcal.HDate(engDate));
+								}
+							</script>
+							  
+							<script>
+							if(!<?php echo($date)?> == '') {
+								document.write(' - ')
+								engDate.setDate(engDate.getDate() + 1);
+								document.write(Hebcal.HDate(engDate));
+							}
+					    </script>
+						</div>
+						<div class="col-md-4"></div>
 					</div>
 				</div>
+				<!-- end upcoming days -->
+				<!-- past flow dates -->
 				<div class="panel panel-default">
 					<div class="panel-heading">Past Flow Dates</div>
-				<table class="table table-bordered table-hover">
-					<tr>
-						<th>Flow Date</th>
-						<th>Night or Day</th>
-						<th>Chodesh</th>
-						<th>Haflagah</th>
-						<th>Onah Beinoni</th>
-					</tr>
-					<?php
-						$checkFlow-> bindParam(':userId', $_SESSION['userId']);
-						$checkFlow-> execute();
-						$data = $checkFlow->fetchAll();
-					?>	
-						<?php foreach ($data as $row): ?>
+					<table class="table table-bordered table-hover">
 						<tr>
-    					<td><script> document.write(Hebcal.HDate(<?php $row['flow_date'] ?>))</script></td>
-    					<td><?php echo($row['day_night']) ?></td>
-    					<td></td>
-    					<td></td>
-    					<td></td>
-    					<?php endforeach; ?>		
+							<th>Flow Date</th>
+							<th>Night or Day</th>
+							<th>Chodesh</th>
+							<th>Haflagah</th>
+							<th>Onah Beinoni</th>
 						</tr>
-				</table>
+						<?php
+							$checkFlow-> bindParam(':userId', $_SESSION['userId']);
+							$checkFlow-> execute();
+							$data = $checkFlow->fetchAll();
+						?>	
+							<?php foreach ($data as $row): ?>
+							<?php $date = json_encode($row['flow_date']); ?>
+							<script>var hebDate = new Hebcal.HDate(new Date(<?php echo($date); ?>));</script>
+	      			<script>var engDate = new Date(<?php echo($date)?>);</script>
+	      			
+							<tr>
+	    					<td><script>document.write(new Hebcal.HDate(new Date(<?php echo($date) ?>)));</script></td>
+	    					<td><?php echo($row['day_night']) ?></td>
+	    					<td><script>document.write(hebDate.getMonthObject().next().getDay(hebDate.getDate()).toString());</script></td>
+	    					<td>
+	    					</td>
+	    					<td>
+		    					<script>
+										engDate.setDate(engDate.getDate() + 29);
+										document.write(Hebcal.HDate(engDate));
+										document.write(' - ');
+										engDate.setDate(engDate.getDate() + 1);
+										document.write(Hebcal.HDate(engDate));
+		      				</script>
+	    					</td>
+	    					<?php endforeach; ?>		
+							</tr>
+					</table>
 				</div>
+				<!-- end past flow dates -->
 			</div>
 			<!-- -->
       <div class="col-md-3 text-center">
