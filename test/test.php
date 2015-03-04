e1369a6f6da5d438332d54a12fab2ce26c234b3f
tzvi <tes5884@gmail.com>
2015-03-04 09:49:43 -0500

    testing

--- test/test.php
+++ test/test.php
@@ -1,22 +1,38 @@
+<script src="../js/hebcal.noloc.min.js"></script>
 <?php 
   include_once ('../php/init.php');
 
   $checkFlow-> bindParam(':userId', $_SESSION['userId']);
 	$checkFlow-> execute();
 	$data = $checkFlow->fetchAll();
-
-  foreach ($data as $row) {
-    echo('<li>');
-    echo($row['flow_date']);
-    echo('</li>');
-  }
 ?>
 
-<script src="../js/hebcal.noloc.min.js"></script>
-<script>
-		document.write(new Hebcal.HDate(new Date('2015-03-27 00:00:00')));
-</script>
-
-<!--
-document.write(new Hebcal.HDate(<?php $row['flow_date'] ?>));
--->
+  <?php foreach ($data as $row): ?>
+    
+    <li>
+    	<!--json encoded date -->
+      <?php $date = json_encode($row['flow_date']); ?>
+      <script>var hebDate = new Hebcal.HDate(new Date(<?php echo($date); ?>));</script>
+      <script>var engDate = new Date(<?php echo($date)?>);</script>
+      
+      <!-- date in hebrew -->
+    	<script>document.write(hebDate.toString());</script>
+  	</li>  	
+  	<li>  	
+    	<!-- date + 1 month -->
+    	<script>document.write(hebDate.getMonthObject().next().getDay(hebDate.getDate()).toString());</script>
+    </li>
+    <li>
+    <!-- 30 and 31 -->
+      <script>
+				engDate.setDate(engDate.getDate() + 29);
+				document.write(Hebcal.HDate(engDate));
+      </script>
+    </li>
+    <li>
+    	<script>
+    		engDate.setDate(engDate.getDate() + 1);
+				document.write(Hebcal.HDate(engDate));
+			</script>	
+    </li>
+<?php endforeach; ?>
