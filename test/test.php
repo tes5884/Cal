3911bf5e360703c192576b02fc3761ccf3a31696
tzvi <tes5884@gmail.com>
2015-03-10 13:25:26 -0400

    test

--- test/test.php
+++ test/test.php
@@ -1,15 +1,38 @@
 <script src="../js/hebcal.noloc.min.js"></script>
 <?php 
   include_once ('../php/init.php');
-  $latestFlow-> bindParam(':userId', $_SESSION['userId']);
-	$latestFlow-> execute();
-	$data = $latestFlow->fetchObject();
+  $checkFlowAsc-> bindParam(':userId', $_SESSION['userId']);
+	$checkFlowAsc-> execute();
+	$data = $checkFlowAsc->fetchAll(); 
+	$prev = "";
+
+	  foreach ($data as $row) { 
+  	  if (!$prev == "") {
+  	    
+  	    $last = new DateTime($prev);
+  	    $current = new DateTime($row['flow_date']);
+  	    $interval = ($last->diff($current));
+				$newDay = date_format(date_add($current, $interval), 'Y-m-d H:i:s');  	    
+  	    
+  	    echo 'previous date: ' . $prev . '<br />';
+				echo 'current date: ' . $row['flow_date'] . '<br />';
+				echo 'interval: ' . $interval->days . '<br />';
+				echo 'haflagah: ' . $newDay . '<br />';
+				echo ' - - - - - - - - - ' . '<br />';
+				
+  	  } else {
+  	    echo 'current date: ' . $row['flow_date'] . '<br />';
+				echo ' - - - - - - - - - ' . '<br />';
+  	  }
+  	  $prev = $row['flow_date'];			
+	  }  
 ?>
-	
+<?php
+/*
 	<!--json encoded date -->
-  <?php $date = json_encode($data->flow_date); ?>
-  <script>var hebDate = new Hebcal.HDate(new Date(<?php echo($date); ?>));</script>
-  <script>var engDate = new Date(<?php echo($date)?>);</script>
+  	<?php $date = json_encode($data->flow_date); ?>
+  	<script>var hebDate = new Hebcal.HDate(new Date(<?php echo($date); ?>));</script>
+  	<script>var engDate = new Date(<?php echo($date)?>);</script>
 	
 	<li>Hebdate - <script>document.write(hebDate.toString());</script></li>
 	<li>Chodedsh - <script>document.write(hebDate.getMonthObject().next().getDay(hebDate.getDate()).toString());</script></li>
@@ -25,11 +48,6 @@
 				document.write(Hebcal.HDate(engDate));
 			</script>
 	</li>
-  
-
-
-<?php
-/*
 <?php foreach ($data as $row): ?>
     <?php echo('<li>' . $row['flow_date'] . '</li>'); ?>
 
